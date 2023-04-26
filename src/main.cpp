25df6cf782b86434ae1e61a9404347bbdc6f0dd5
Pranav Sukumar <pranavsukumar@pranav21.local>
2023-04-26 09:14:47 -0700

    can add duplicate objects to scene

--- src/main.cpp
+++ src/main.cpp
@@ -169,13 +169,6 @@ bool loadObjectsFromFile(string filename, WaterCube *waterCube, WaterCubeParamet
   for (json::iterator it = j.begin(); it != j.end(); ++it) {
     string key = it.key();
 
-    // Check that object is valid
-    unordered_set<string>::const_iterator query = VALID_KEYS.find(key);
-    if (query == VALID_KEYS.end()) {
-      cout << "Invalid scene object found: " << key << endl;
-      exit(-1);
-    }
-
     // Retrieve object
     json object = it.value();
 
@@ -236,7 +229,7 @@ bool loadObjectsFromFile(string filename, WaterCube *waterCube, WaterCubeParamet
       cp->density = density;
       cp->damping = damping;
       cp->ks = ks;
-  }else if (key == SPHERE) {
+  } else if (key.substr(0, 6) == SPHERE) {
       Vector3D origin;
       double radius, friction;
 
